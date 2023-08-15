f1e23cc05a4b9b4972e45336331bbb0de9f3600a
Raman <241ramankumar@gmail.com>
2023-08-15 10:54:43 +0530

    W-1  1st assignment

---
+++ week-1/q5.cpp
@@ -0,0 +1,13 @@
+
+// Write a program to find the circumference of a circle with radius 10 in C++.
+#include<iostream>
+using namespace std;
+
+
+ int main(){
+    float pi=3.14;
+     int radius=10;
+    cout<<"circumference of circle is "<<endl;
+    cout<<2*pi*radius<<endl;
+    return 0;
+ }
